d5e7ad2b5fb7cbef75fef79067ac87d5fd13af89
sjp07 <jpsact@yahoo.com>
2018-01-14 15:53:11 -0700

    Added welcome slides and paging

---
+++ components/Slides.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { View, Text, ScrollView, Dimensions } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+class Slides extends Component {
+  renderSlides() {
+    return this.props.data.map((slide) => {
+      return (
+        <View key={slide.text} style={styles.slideStyle}>
+          <Text style={styles.textStyle}>{slide.text}</Text>
+        </View>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <ScrollView
+        horizontal
+        style={{ flex: 1 }}
+        pagingEnabled
+      >
+        {this.renderSlides()}
+      </ScrollView>
+    );
+  }
+}
+
+const styles = {
+    slideStyle: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: SCREEN_WIDTH
+    },
+    textStyle: {
+      fontSize: 30
+    }
+};
+
+export default Slides;
